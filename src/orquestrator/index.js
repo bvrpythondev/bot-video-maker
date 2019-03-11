81521bc1eb5cee36fc18c30dd545fc3f72951ac4
Tauan Matos <tauan.matos164@gmail.com>
2019-03-11 17:08:42 -0300

    Modularização do userInput

--- src/orquestrator/index.js
+++ src/orquestrator/index.js
@@ -1,21 +1,12 @@
-const readline = require('readline-sync')
-
-function start() {
+const robots = {
+  userInput: require('../user-input/user-input'),
+  text: require('../text-robot/text.js')
+}
+async function start() {
   const content = {}
 
-  content.searchTerm = askAndReturnSearchTerm()
-  content.Prefix = askAndReturnPrefix()
-
-  function askAndReturnSearchTerm() {
-    return readline.question("Type a Wikipedia search term: ")
-  }
-
-  function askAndReturnPrefix(){
-    const prefixes = ['Who is', 'What is', 'The history of']
-    const selectPrefixIndex = readline.keyInSelect(prefixes)
-    const selectPrefixText = prefixes[selectPrefixIndex]
-    return selectPrefixText;
-  }
+  robots.userInput(content)
+  await robots.text(content)
 
   console.log(content)
 
